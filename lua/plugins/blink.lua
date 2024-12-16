6a917a43d758030d5d6a84d5f1cfccc72faa4044
Jin Heo <993jin@gmail.com>
2024-12-16 00:59:23 -0500

    update blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -35,7 +35,7 @@ return {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
         -- Useful for when your theme doesn't support blink.cmp
         -- will be removed in a future release
-        use_nvim_cmp_as_default = true,
+        use_nvim_cmp_as_default = false,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "normal",
@@ -47,6 +47,51 @@ return {
         default = { "lsp", "path", "snippets", "buffer" },
         -- optionally disable cmdline completions
         -- cmdline = {},
+      },
+      fuzzy = {
+        use_frecency = false,
+        use_proximity = false,
+        sort = {},
+      },
+      trigger = {
+        completion = {
+          -- When true, will prefetch the completion items when entering insert mode
+          -- WARN: buggy, not recommended unless you'd like to help develop prefetching
+          prefetch_on_insert = false,
+          -- When false, will not show the completion window automatically when in a snippet
+          show_in_snippet = false,
+          -- When true, will show the completion window after typing a character that matches the `keyword.regex`
+          show_on_keyword = false,
+          -- When true, will show the completion window after typing a trigger character
+          show_on_trigger_character = false,
+          -- LSPs can indicate when to show the completion window via trigger characters
+          -- however, some LSPs (i.e. tsserver) return characters that would essentially
+          -- always show the window. We block these by default.
+          show_on_blocked_trigger_characters = function()
+            if vim.api.nvim_get_mode().mode == "c" then
+              return {}
+            end
+            -- you can also block per filetype, for example:
+            -- if vim.bo.filetype == 'markdown' then
+            --   return { ' ', '\n', '\t', '.', '/', '(', '[' }
+            -- end
+            return { " ", "\n", "\t" }
+          end,
+          -- When both this and show_on_trigger_character are true, will show the completion window
+          -- when the cursor comes after a trigger character after accepting an item
+          show_on_accept_on_trigger_character = true,
+          -- When both this and show_on_trigger_character are true, will show the completion window
+          -- when the cursor comes after a trigger character when entering insert mode
+          show_on_insert_on_trigger_character = true,
+          -- List of trigger characters (on top of `show_on_blocked_trigger_characters`) that won't trigger
+          -- the completion window when the cursor comes after a trigger character when
+          -- entering insert mode/accepting an item
+          show_on_x_blocked_trigger_characters = { "'", '"', "(" },
+          -- or a function, similar to show_on_blocked_trigger_character
+        },
+        signature_help = {
+          enabled = true,
+        },
       },
 
       -- experimental signature help support
