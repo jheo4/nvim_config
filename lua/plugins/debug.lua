04c96c9849a7791326abd44eff6f01cff1b79b30
Jin Heo <993jin@gmail.com>
2026-03-04 13:10:33 -0500

    update

--- lua/plugins/debug.lua
+++
@@ -1,8 +0,0 @@
--- This file is for debugging purposes. It can be used to disable plugins or make other temporary changes without affecting the main configuration.
--- On Mac, command renderer (noice) crashes nvim. This is a temporary workaround to disable it until a fix is found - 03-04-2026
-return {
-  {
-    "folke/noice.nvim",
-    enabled = false,
-  },
-}
