4c449f42e443a193f8d8d9798ce883b31c411fa7
Jin Heo <993jin@gmail.com>
2026-06-10 16:58:44 -0400

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+  },
+  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
@@ -7,6 +13,23 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+        callback = function()
+          local f = vim.fn.expand("%:p")
+          if vim.fn.isdirectory(f) == 1 then
+            vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+            require("neo-tree")
+          end
+        end,
+      })
+    end,
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = "open_default",
+      },
     },
   },
 }
